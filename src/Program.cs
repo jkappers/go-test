65044eaaf7b29e2377469970b811e0bcc26ed6b7
Joshua Kappers <josh@jkappers.me>
2025-12-03 08:20:31 -0600

    feat: update workflows

--- src/Program.cs
+++ src/Program.cs
@@ -6,3 +6,6 @@ var app = builder.Build();
 app.MapGet("/", () => $"Hello from {Environment.MachineName}\n");
 app.MapGet("/health", () => Results.Ok());
 app.Run();
+
+// Make Program accessible to test project
+public partial class Program { }
